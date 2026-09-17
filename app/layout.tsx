8df3463c5ea18a299aa382671f863315c9d7f326
Iqbal Singh <148329082+Iqbalsingh1988@users.noreply.github.com>
2026-09-17 18:51:33 +0530

    Add RootLayout component with metadata for app

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const metadata = {
+  title: "SonicStudio AI - Music Generator",
+  description: "Generate AI Music easily",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
